d5dd38e8e259dfce733909b161afed2538bb5e76
jshilliard08 <73717438+jshilliard08@users.noreply.github.com>
2021-02-09 19:43:50 -0500

    updating index.html

---
+++ css/script.js
@@ -0,0 +1,41 @@
+//List of all your questions chaoices and answers 
+var questions = [
+    {
+        title: "JavaScript is a ___ -side programming language.",
+        choices: ["client", "server", "both", "none"],
+        answer: "blue"
+    }
+    {   title: ""
+    
+
+    }
+]
+
+
+// write logic for application
+
+
+
+// variables to keep track of the quiz state
+
+// variables to referance DOM elements
+
+// function to start quiz
+
+// function to check the choice to the answer
+//if else statement to check
+
+
+
+// function to end the quiz and tally up the score
+
+
+// function to save high score to high score page
+// hint (window.location.href)
+
+//might want to make another JS page
+//function that prints highscores to hight score page
+
+
+
+
